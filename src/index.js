f65da0144ec5f0719517132a790c064976bfc42e
hamzayounes858@yahoo.com <hamzayounes858@yahoo.com>
2021-12-20 11:29:00 +0200

    The final commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import reducer from './reducers' //That will call the file of index.js (a default behavior)
 import middleware from './middleware' //The same technique as above
 import App from './components/App'
 import './index.css'
+import thunk from 'redux-thunk'
+import logger from './middleware/logger'
+import checker from './middleware/checker'
 
-const store = createStore(reducer, middleware);
+const store = createStore(reducer, applyMiddleware(thunk, logger, checker));
 
 ReactDOM.render(
     <Provider store= {store}>
